bb746db4d434e2616ec61a7e2327cdb136571dc2
Christian LeVesque <christian@christianlevesque.io>
2020-12-17 11:34:35 -0600

    Updated GetShortUrl to return a redirection if the short url was valid

--- Tests/Controllers/UrlController_GetShortUrl.cs
+++ Tests/Controllers/UrlController_GetShortUrl.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Webapp.Controllers;
 using Xunit;
@@ -7,6 +8,8 @@ namespace Tests.Controllers
 {
 	public class UrlController_GetShortUrl
 	{
+		private const string Url = "https://www.christianlevesque.io/blog/fcc";
+		
 		[Fact]
 		public void GetShortUrlReturnsNotFoundWhenIndexOutOfRange()
 		{
@@ -15,6 +18,19 @@ namespace Tests.Controllers
 			var result = controller.GetShortUrl(0);
 
 			Assert.IsType<NotFoundResult>(result);
+		}
+
+		[Fact]
+		public async Task GetShortUrlReturnsRedirectResultWhenShortUrlIsValid()
+		{
+			var controller = new UrlController();
+
+			await controller.CreateShortUrl(Url);
+
+			var result = controller.GetShortUrl(0);
+
+			var response = Assert.IsType<RedirectResult>(result);
+			Assert.Equal(Url, response.Url);
 		}
 	}
 }
