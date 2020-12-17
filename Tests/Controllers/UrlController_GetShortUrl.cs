b023df4d444b0491acc3aed7dcb7b4989c297891
Christian LeVesque <christian@christianlevesque.io>
2020-12-16 20:10:08 -0600

    Added GetShortUrl method to UrlController with unit test

---
+++ Tests/Controllers/UrlController_GetShortUrl.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Webapp.Controllers;
+using Xunit;
+
+namespace Tests.Controllers
+{
+	public class UrlController_GetShortUrl
+	{
+		[Fact]
+		public void GetShortUrlReturnsBadRequestWhenIndexOutOfRange()
+		{
+			var controller = new UrlController();
+
+			var result = controller.GetShortUrl(0);
+
+			var response = Assert.IsType<BadRequestObjectResult>(result);
+			Assert.NotNull(response.Value);
+			Assert.IsType<Dictionary<string, string>>(response.Value);
+			Assert.Equal((response.Value as IDictionary<string, string>).Keys.Count, 1);
+			Assert.True((response.Value as IDictionary<string, string>).ContainsKey("error"));
+			Assert.Equal("invalid url", (response.Value as IDictionary<string, string>)["error"]);
+		}
+	}
+}
--- Webapp/Controllers/UrlController.cs
+++ Webapp/Controllers/UrlController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Webapp.Controllers
@@ -6,9 +7,18 @@ namespace Webapp.Controllers
 	[Route("/shorturl")]
 	public class UrlController : ControllerBase
 	{
+		private readonly IDictionary<string, string> _error = new Dictionary<string, string>
+		{
+			{"error", "invalid url"}
+		};
+		private readonly IList<string> _urls = new List<string>();
+		
 		[HttpGet("{index}")]
 		public IActionResult GetShortUrl(int index)
 		{
+			if (_urls.Count <= index)
+				return BadRequest(_error);
+
 			return Ok();
 		}
 	}
