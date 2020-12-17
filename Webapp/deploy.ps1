c57999dd67f1fc0192f5e77f3049439f89b4992e
Christian LeVesque <christian@christianlevesque.io>
2020-12-17 09:25:04 -0600

    Fixed name of microservice

--- Webapp/deploy.ps1
+++ Webapp/deploy.ps1
@@ -95,4 +95,4 @@ Write-Host "...done"
 ### Done ###
 ############
 
-Write-Host "To run Timestamp, log into remote server and execute ~/$build_dir/url-shortener"
+Write-Host "To run URL Shortener, log into remote server and execute ~/$build_dir/url-shortener"
