9c49907f8ff87104390da71c02f8b84e7f54ed7c
Christian LeVesque <christian@christianlevesque.io>
2020-12-16 19:23:56 -0600

    Added routing data and GetShortUrl argument

--- Webapp/Controllers/UrlController.cs
+++ Webapp/Controllers/UrlController.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 namespace Webapp.Controllers
 {
 	[ApiController]
+	[Route("/shorturl")]
 	public class UrlController : ControllerBase
 	{
-		public IActionResult GetShortUrl()
+		[HttpGet("{index}")]
+		public IActionResult GetShortUrl(int index)
 		{
 			return Ok();
 		}
